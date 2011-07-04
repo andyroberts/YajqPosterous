79ef2fd5bd5f0ac1efa314eb31597bc4bc897cf7
Andrew Roberts <andy.roberts.uk@remove_gmail_me.com>
2011-07-04 18:42:05 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,71 @@
+# YajqPosterous - Yet another jQuery Posterous plugin
+
+(Last updated 30th June 2011)
+
+## Description
+
+YajqTwitter is a jQuery plugin for retrieving posts via the official Posterous API.
+The objective is to create a simple blog feed widget displaying snippets of posts to embed in webpages.
+And with CSS it's possible customize the appearance too.
+
+## Usage
+
+1\. Link to required Javascript files. Naturally, as it's a jQuery plugin, you'll need to link to jQuery too.
+
+```html
+...
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script type="text/javascript" src="yajqposterous.js"></script>
+...
+```
+
+2\. Insert the following Javascript: 
+
+```html
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('#blogFeed').yajqposterous({username: 'your_posterous_username'});
+    });
+  </script>
+```
+
+3\. Ensure the selector exists. In this instance you need to place the following HTML into the document body:
+
+```html
+  <div id="blogFeed"></div>
+```
+
+YajqPosterous will populate this div with the items from your specified Posterous feed.
+
+By default it will retrieve five posts from the specified feed. To change this value, set the 'maxNumberOfPosts' option too, e.g.:
+
+```html
+  $('#blogFeed').yajqposterous({username: 'your_posterous_username', maxNumberOfPosts: 10});
+```
+
+See posterous_test.html for sample use and styling.
+
+## Roadmap
+
+1. Use setInterval to check with Posterous for new posts.
+
+2. Include reasonable error message when API is not available.
+
+3. Make fuzzy timestamps an option.
+
+## Author
+
+Andrew Roberts
+
+Twitter: [@andyroberts_uk](http://twitter.com/andyroberts_uk)
+
+Web: http://www.andy-roberts.net
+
+Github: https://github.com/andyroberts
+
+## License
+
+(c) 2011 Andrew Roberts
+
+Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php). 
+
---
+++ yajqposterous.js
@@ -0,0 +1,92 @@
+/* YajqPosterous.js - Yet Another jQuery Posterous plugin
+ * (c) 2011 Andrew Roberts - http://www.andy-roberts.net
+ * Released under MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+(function($) {
+
+	$.fn.yajqposterous = function(options) { 
+
+		options = $.extend({}, $.fn.yajqposterous.defaults, options);
+
+		/* Credit: jquery.TimeAgo.js */ 
+		var zeropad = function (num) {
+		  return ((num < 10) ? '0' : '') + num;
+		};
+		
+		/* Credit: jquery.TimeAgo.js */ 
+		var iso8601 = function (date) {
+		  return date.getUTCFullYear()
+		    + "-" + zeropad(date.getUTCMonth()+1)
+		    + "-" + zeropad(date.getUTCDate())
+		    + "T" + zeropad(date.getUTCHours())
+		    + ":" + zeropad(date.getUTCMinutes())
+		    + ":" + zeropad(date.getUTCSeconds()) + "Z";
+		};
+
+		/* Credit: John Resig (http://ejohn.org/blog/javascript-pretty-date/) */ 
+		var prettyDate = function(time) {
+			var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+			diff = (((new Date()).getTime() - date.getTime()) / 1000),
+			day_diff = Math.floor(diff / 86400);
+			
+			if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+				return;
+			
+			return day_diff == 0 && (
+				diff < 60 && "just now" ||
+				diff < 120 && "1 minute ago" ||
+				diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+				diff < 7200 && "1 hour ago" ||
+				diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+				day_diff == 1 && "Yesterday" ||
+				day_diff < 7 && day_diff + " days ago" ||
+				day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+		}
+		
+
+		var parseFunctions = {
+			'regular': function parseRegular(post) {
+				var postString = "<div class=\"title\"><a href=\""+post['full_url']+"\">" + post['title'] + "</a></div>";
+				var bodyString = post['body_excerpt'];
+				bodyString += " <a href=\""+post['full_url']+"\">[Read more]</a>";
+			
+				postString += "<div class=\"post-body\">" + bodyString + "</div>";
+				return postString;
+			},
+		};
+
+
+		return this.each(function() {
+				var $this = $(this);
+				$.ajax({
+					type:'GET',
+					dataType: "jsonp",
+					url:"http://posterous.com/api/2/sites/" + options.username + "/posts/public",
+					success: function(posts_api_read) {
+			
+						if (posts_api_read == null) {
+						} else {
+							var postsBuffer = "";
+							$.each(posts_api_read, function(i, post){
+								if (i >= options.maxNumberOfPosts) {
+									return false;
+								}
+								var postDate = new Date(post["display_date"]);
+								postsBuffer += "<div class=\"post\">" + parseFunctions["regular"].apply(this, [post]) + "<div class=\"postDate\">"+prettyDate(iso8601(postDate))+"</div></div>";
+							});
+							$this.append("<div class=\"posts\">"+postsBuffer+"</div>");
+						}
+					},
+					error: function() {}
+
+				});
+		});
+	}
+
+	$.fn.yajqposterous.defaults = {
+        	maxNumberOfPosts: 5,
+		username: 'andyroberts-uk'
+	}; 
+
+})(jQuery);
